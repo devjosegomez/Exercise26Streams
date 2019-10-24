0103a900e7d9ab57f44fa640b442b711a97d5bba
devjosegomez <devjosegomez@gmail.com>
2019-10-24 11:51:49 -0500

    Initial commit

---
+++ src/com/exercise26streams/app/WriterApp.java
@@ -0,0 +1,11 @@
+package com.exercise26streams.app;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+public class WriterApp {
+
+	public static void main(String[] args) {
+		File myFile = null;
+	}	
+
+}
