0eab7a11c0edb412f25f569ae29c7f891ab9d2c5
devjosegomez <devjosegomez@gmail.com>
2019-10-24 12:22:42 -0500

    FileWriter examples

--- src/com/exercise26streams/app/WriterApp.java
+++ src/com/exercise26streams/app/WriterApp.java
@@ -9,25 +9,23 @@ public class WriterApp {
 
 	public static void main(String[] args) {
 		int table =0;
-		final int LIMIT = 100;
+		final int LIMIT = 10;
 		File myFile = null;
 		FileWriter myWriter = null;
 		BufferedWriter myBuffer = null;
 		Scanner input = new Scanner(System.in);
-		table = input.nextInt();
-		
 		
 		try {
 			myFile = new File("c:\\a\\b.txt");
-			myWriter = new FileWriter(myFile, true); //true - Append /false - Overwrite
+			myWriter = new FileWriter(myFile, false); //true - Append /false - Overwrite
 			myBuffer = new BufferedWriter(myWriter);
 			
 			System.out.println("Enter a number to multiply:");
 			table = input.nextInt();
 			
-			for(int i =0; i<LIMIT; i++) {
-				System.out.println(String.format("%d * %d = %d", table, i, table * i ));
-				myBuffer.write("Jose app ");
+			for(int i =1; i<=LIMIT; i++) {
+				System.out.println(String.format("%dX%d=%d", table, i, table * i ));
+				myBuffer.write(String.format("[%dX%d=%d] \n", table, i, table * i ));
 			}
 			
 			System.out.println("File was created");
