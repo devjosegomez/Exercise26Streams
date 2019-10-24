73cf6b72d789e2e42f5e51577ec2c4c313189701
devjosegomez <devjosegomez@gmail.com>
2019-10-24 11:59:49 -0500

    Streams write in a file

--- src/com/exercise26streams/app/WriterApp.java
+++ src/com/exercise26streams/app/WriterApp.java
@@ -1,11 +1,32 @@
 package com.exercise26streams.app;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.BufferedWriter;
 public class WriterApp {
 
 	public static void main(String[] args) {
 		File myFile = null;
+		FileWriter myWriter = null;
+		BufferedWriter myBuffer = null;
+
+		try {
+			myFile = new File("c:\\a\\b.txt");
+			myWriter = new FileWriter(myFile, true); //Append 
+			myBuffer = new BufferedWriter(myWriter);
+			myBuffer.write("Jose app ");
+			System.out.println("File was created");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				myBuffer.close();
+				myWriter = null;
+				myFile = null;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}	
 
 }
